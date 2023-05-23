29a7759373cfef7d076a8f6573b5a8ec9f793a69
Shincy <shincyshnz@gmail.com>
2023-05-23 09:16:18 +0530

    last commmit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,40 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 export function Counter({ count, onIncrement, onDecrement }) {
+  const [userList, setUserList] = useState([]);
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ name: "ghi" }),
+  };
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch("http://localhost:3005/users");
+      const data = await response.json();
+      // console.log(data);
+      setUserList(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={onIncrement}>Increment</button>
-      <button onClick={onDecrement}>Decrement</button>
+      <button onClick={onDecrement}>-</button>
+      <button onClick={onIncrement}>+</button>
+
+      <h1>User List</h1>
+      <ul>
+        {userList.map((user, index) => (
+          <li key={index}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
